97ac90cdb8e33b28be43049e14e7ac45e24a9b27
Roberto <roberto.infante03@gmail.com>
2021-04-07 19:55:42 -0700

    added bb court

--- dist/index.3fafb3e2.js
+++ dist/index.3fafb3e2.js
@@ -26273,16 +26273,212 @@ try {
   var _react = require("react");
   var _reactDefault = _parcelHelpers.interopDefault(_react);
   var _jsxFileName = "/Users/infa/Documents/INFO/INFO474/uw_mbb_vis/src/App.js";
+  const viewHeight = 500;
+  const viewWidth = 500;
   const App = () => {
     return (
-      /*#__PURE__*/_reactDefault.default.createElement("div", {
+      /*#__PURE__*/_reactDefault.default.createElement("svg", {
+        width: "750",
+        height: "831",
+        viewBox: "0 0 750 831",
+        fill: "none",
+        xmlns: "http://www.w3.org/2000/svg",
         __self: undefined,
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 4,
+          lineNumber: 7,
           columnNumber: 10
         }
-      }, "hello world from react")
+      }, /*#__PURE__*/_reactDefault.default.createElement("rect", {
+        x: "1.5",
+        y: "116.5",
+        width: "747",
+        height: "713",
+        fill: "#262626",
+        stroke: "black",
+        "stroke-width": "3",
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 8,
+          columnNumber: 3
+        }
+      }), /*#__PURE__*/_reactDefault.default.createElement("rect", {
+        x: "45",
+        y: "115",
+        width: "660",
+        height: "380",
+        fill: "#D90429",
+        "fill-opacity": "0.7",
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 9,
+          columnNumber: 3
+        }
+      }), /*#__PURE__*/_reactDefault.default.createElement("circle", {
+        cx: "375",
+        cy: "352.5",
+        r: "360.5",
+        fill: "#D90429",
+        stroke: "black",
+        "stroke-width": "3",
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 10,
+          columnNumber: 3
+        }
+      }), /*#__PURE__*/_reactDefault.default.createElement("circle", {
+        cx: "375",
+        cy: "405",
+        r: "88.5",
+        fill: "#793A3A",
+        stroke: "black",
+        "stroke-width": "3",
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 11,
+          columnNumber: 3
+        }
+      }), /*#__PURE__*/_reactDefault.default.createElement("rect", {
+        x: "253.5",
+        y: "116.5",
+        width: "242",
+        height: "287",
+        fill: "#793A3A",
+        stroke: "black",
+        "stroke-width": "3",
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 12,
+          columnNumber: 3
+        }
+      }), /*#__PURE__*/_reactDefault.default.createElement("rect", {
+        x: "286.5",
+        y: "116.5",
+        width: "177",
+        height: "287",
+        fill: "#D90429",
+        stroke: "black",
+        "stroke-width": "3",
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 13,
+          columnNumber: 3
+        }
+      }), /*#__PURE__*/_reactDefault.default.createElement("circle", {
+        cx: "375",
+        cy: "194",
+        r: "61",
+        fill: "#C4C4C4",
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 14,
+          columnNumber: 3
+        }
+      }), /*#__PURE__*/_reactDefault.default.createElement("line", {
+        x1: "330.015",
+        y1: "173.5",
+        x2: "420.011",
+        y2: "174.409",
+        stroke: "black",
+        "stroke-width": "3",
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 15,
+          columnNumber: 3
+        }
+      }), /*#__PURE__*/_reactDefault.default.createElement("rect", {
+        x: "6",
+        y: "193",
+        width: "39",
+        height: "319",
+        fill: "#262626",
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 16,
+          columnNumber: 3
+        }
+      }), /*#__PURE__*/_reactDefault.default.createElement("rect", {
+        x: "705",
+        y: "176",
+        width: "37",
+        height: "319",
+        fill: "#262626",
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 17,
+          columnNumber: 3
+        }
+      }), /*#__PURE__*/_reactDefault.default.createElement("rect", {
+        width: "750",
+        height: "115",
+        fill: "white",
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 18,
+          columnNumber: 3
+        }
+      }), /*#__PURE__*/_reactDefault.default.createElement("rect", {
+        x: "45",
+        y: "115",
+        width: "80",
+        height: "380",
+        fill: "#D90429",
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 19,
+          columnNumber: 3
+        }
+      }), /*#__PURE__*/_reactDefault.default.createElement("rect", {
+        x: "625",
+        y: "115",
+        width: "80",
+        height: "380",
+        fill: "#D90429",
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 20,
+          columnNumber: 3
+        }
+      }), /*#__PURE__*/_reactDefault.default.createElement("line", {
+        x1: "706.5",
+        y1: "495",
+        x2: "706.5",
+        y2: "115",
+        stroke: "black",
+        "stroke-width": "3",
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 21,
+          columnNumber: 3
+        }
+      }), /*#__PURE__*/_reactDefault.default.createElement("line", {
+        x1: "46.516",
+        y1: "500.003",
+        x2: "46.516",
+        y2: "115",
+        stroke: "black",
+        "stroke-width": "3",
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 22,
+          columnNumber: 3
+        }
+      }))
     );
   };
   _c = App;
--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
 
+const viewHeight = 500;
+const viewWidth = 500;
+
 const App = () => {
-  return <div>hello world from react</div>;
+  return <svg width="750" height="831" viewBox="0 0 750 831" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect x="1.5" y="116.5" width="747" height="713" fill="#262626" stroke="black" stroke-width="3"/>
+  <rect x="45" y="115" width="660" height="380" fill="#D90429" fill-opacity="0.7"/>
+  <circle cx="375" cy="352.5" r="360.5" fill="#D90429" stroke="black" stroke-width="3"/>
+  <circle cx="375" cy="405" r="88.5" fill="#793A3A" stroke="black" stroke-width="3"/>
+  <rect x="253.5" y="116.5" width="242" height="287" fill="#793A3A" stroke="black" stroke-width="3"/>
+  <rect x="286.5" y="116.5" width="177" height="287" fill="#D90429" stroke="black" stroke-width="3"/>
+  <circle cx="375" cy="194" r="61" fill="#C4C4C4"/>
+  <line x1="330.015" y1="173.5" x2="420.011" y2="174.409" stroke="black" stroke-width="3"/>
+  <rect x="6" y="193" width="39" height="319" fill="#262626"/>
+  <rect x="705" y="176" width="37" height="319" fill="#262626"/>
+  <rect width="750" height="115" fill="white"/>
+  <rect x="45" y="115" width="80" height="380" fill="#D90429"/>
+  <rect x="625" y="115" width="80" height="380" fill="#D90429"/>
+  <line x1="706.5" y1="495" x2="706.5" y2="115" stroke="black" stroke-width="3"/>
+  <line x1="46.516" y1="500.003" x2="46.516" y2="115" stroke="black" stroke-width="3"/>
+  </svg>
+  
 };
 
 export default App;
