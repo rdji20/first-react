2264b1efb04ea4fc6d701be369a96a9aa26fac91
Roberto <roberto.infante03@gmail.com>
2021-06-02 23:06:36 -0700

    Add example players

--- src/App.js
+++ src/App.js
@@ -65,6 +65,8 @@ const App = () => {
     min: minDraymond,
   };
 
+  const [listPlayers, setListPlayers] = useState(false);
+
   return (
     <div>
       <h1 id="title-shw">SHOTwer comparison shot between players</h1>
@@ -73,6 +75,41 @@ const App = () => {
         This software contains data from all the shots attempted by all the
         players in the 2018-19 NBA season. A query example is player1 = "Damian
         Lillard" and player2 "Markelle Fultz" (Expected name as NBA database).
+      </p>
+      <button
+        className="center-btn"
+        onClick={(e) => {
+          if (!listPlayers) {
+            e.target.textContent = "Hide list";
+            setListPlayers(true);
+          } else {
+            e.target.textContent = "Show list";
+            setListPlayers(false);
+          }
+        }}
+      >
+        {" "}
+        Click here to see a list of player names
+      </button>
+      <p className={listPlayers ? "" : "hidden"}>
+        <span id="bold-big"> Exclude the number: </span> 125. Justise Winslow
+        124. Larry Nance Jr. 123. Cody Zeller 122. Kyle Anderson 121. Rajon
+        Rondo 120. Jeremy Lamb 119. Kelly Olynyk 118. Nicolas Batum 117. Terry
+        Rozier 116. Lonzo Ball 115. Rudy Gay 114. Tyreke Evans 113. Patty Mills
+        112. Tim Hardaway Jr. 111. Evan Fournier 110. Jae Crowder 109. Eric
+        Gordon 108. Dario Saric 107. Reggie Jackson 106. Al Farouq-Aminu 105.
+        Jaren Jackson Jr. 104. Fred VanVleet 103. Enes Kanter 102. Danny Green
+        101. Gordon Hayward 100. Otto Porter Jr. 99. Lauri Markkanen 98. Caris
+        LeVert 97. Marcus Smart 96. Dwyane Wade 95. Dennis Schroder 94. Goran
+        Dragic 93. Darren Collison 92. Jarrett Allen 91. Ricky Rubio 90. Bogdan
+        Bogdanovic 89. Derrick Favors 88. Brook Lopez 87. Nikola Mirotic 86.
+        Jaylen Brown 85. Jerami Grant 84. Spencer Dinwiddie 83. Jeff Teague 82.
+        Bojan Bogdanovic 81. TJ Warren 80. D'Angelo Russell 79. Harrison Barnes
+        78. Hassan Whiteside 77. Marcus Morris 76. Kevin Love 75. Aaron Gordon
+        74. Joe Ingles 73. Willie Cauley-Stein 72. Jonas Valanciunas 71. Gary
+        Harris 70. Andrew Wiggins 69. Brandon Ingram 68. Kyle Kuzma 67. Paul
+        Millsap 66. JJ Redick 65. Robert Covington 64. Derrick Rose 63. DeAndre
+        Ayton 62. Josh Richardson 61. Malcolm Brogdon
       </p>
       <div>
         <SHOTwer data={allPlayersData} />
